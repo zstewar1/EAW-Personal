2085ff4554df19c2489f4a07b9707f5ec17fb627
Zachary Stewart <zachary@zstewart.com>
2015-11-20 16:58:31 -0500

    more projectile changes

--- scripts/projspeed.py
+++ scripts/projspeed.py
@@ -13,20 +13,75 @@ def get_space_projectiles(projroot):
 
         yield proj
 
-def set_projectile_speeds(projroot):
-    logger.info('Setting projectile speeds')
+def set_projectile_speed(proj):
+    name = proj.get('Name').lower()
 
-    for proj in get_space_projectiles(projroot):
-        logger.info('Changing projectile %s', proj.get('Name'))
-
-        name = proj.get('Name').lower()
-
-        speedtag = proj.find('Max_Speed')
-        if speedtag is not None:
+    speedtag = proj.find('Max_Speed')
+    if speedtag is not None:
+        if 'laser' in name or 'ion' in name:
+            speedtag.text = '120.0'
+            logger.info('Set speed to 120.0')
+        else:
             speedval = float(speedtag.text)
-            newspeed = speedval * 2.5
-            logger.info('Old speed %g, new speed %g', speedval, newspeed)
-            speedtag.text = str(newspeed)
+            newspeed = speedval * 6
+            speedtag.text = str(speedval)
+            logger.info('Set speed to %g (from %g)', newspeed, speedval)
+
+GREEN_LASER_COLOR = '129,255,94,255'
+RED_LASER_COLOR = '255,61,44,255'
+ION_COLOR = '104,181,230,255'
+
+def set_laser_type(proj):
+    name = proj.get('Name').lower()
+
+    if 'laser' not in name:
+        return
+
+    model = proj.find('Space_Model_Name')
+    if model is not None:
+        proj.remove(model)
+
+    texslot = proj.find('Projectile_Texture_Slot')
+    if texslot is not None:
+        texslot.text = '2,0'
+
+    custrend = proj.find('Projectile_Custom_Render')
+    if custrend is not None:
+        custrend.text = '2'
+
+    color = proj.find('Projectile_Laser_Color')
+    if color is None:
+        color = etree.SubElement(proj, 'Projectile_Laser_Color')
+
+    if 'green' in name:
+        color.text = GREEN_LASER_COLOR
+    elif 'red' in name:
+        color.text = RED_LASER_COLOR
+
+def set_proj_appearance(proj):
+    name = proj.get('Name').lower()
+
+    if 'laser' not in name and 'ion' not in name:
+        return
+
+    color = proj.find('Projectile_Laser_Color')
+    if color is not None:
+        if 'green' in name:
+            color.text = GREEN_LASER_COLOR
+        elif 'red' in name:
+            color.text = RED_LASER_COLOR
+        elif 'ion' in name:
+            color.text = ION_COLOR
+
+    width = proj.find('Projectile_Width')
+    if width is not None:
+        widval = float(width.text)
+        width.text = str(widval * 1.75)
+
+    length = proj.find('Projectile_Length')
+    if length is not None:
+        lenval = float(length.text)
+        length.text = str(lenval *  4)
 
 def execute():
     sourcefile = conf.srcxml / 'projectiles.xml'
@@ -34,9 +89,14 @@ def execute():
     with sourcefile.open() as projfile:
         projdata = etree.parse(projfile)
 
-    set_projectile_speeds(projdata.getroot())
+    for proj in get_space_projectiles(projdata.getroot()):
+        logger.info('Changing projectile %s', proj.get('Name'))
+        set_projectile_speed(proj)
+        set_laser_type(proj)
+        set_proj_appearance(proj)
+
 
     destfile = conf.outxml / 'projectiles.xml'
     logger.info('Writing to %s', destfile)
     with destfile.open('wb') as projfile:
-        projdata.write(projfile)
+        projdata.write(projfile, xml_declaration=True, encoding='utf-8')
